a20c148e672d98e978929283d284990e54ee0aaf
Colin <colin.laresebros@gmail.com>
2025-02-22 21:48:17 +0100

    affichage du labyrinteh réussit mais les murs ne se sont pas détruit ce qui créer une grille enfaite. liste des label toujours à regler

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -66,9 +66,10 @@ class Labyrinthe:
             directions = self.__directions_possibles(i, j)
             pile.depiler() if len(directions) == 0 else self.__abattre_mur(i, j, random.choice(directions), pile)
 
+    
 
 class Jeux():
-    def __init__(self, couleur, titre):
+    def __init__(self, couleur, titre, largeur_laby, hauteur_laby):
         pygame.init()
         self.fenetre = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.fenetre.fill(couleur) 
@@ -77,25 +78,52 @@ class Jeux():
         pygame.display.set_icon(self.icon)
         self.clock = pygame.time.Clock()
         self.liste_labels = [] # liste de tuple pour enregistrer les labels
-        self.liste_lignes = [] # liste de tuple pour enregistrer les lignes 
-    def xy_pourcent(self, X, Y): return int(pygame.display.Info().current_w*X*0.01), int(pygame.display.Info().current_h *Y*0.01)
+        self.liste_lignes = [] # liste de tuple pour enregistrer les lignes
+        self.largeur_laby, self.hauteur_laby = largeur_laby, hauteur_laby
+        self.labyrinthe = Labyrinthe(largeur_laby, hauteur_laby)
 
-    def creer_ligne(self, x1, x2, y1, y2, epaisseur, couleur): 
+    # Sert à convertir des pourcentages X, Y en fonction de la taille de l'écran (donc 100 > X,Y > 0)
+    def unite_relatif(self, X, Y): return int(pygame.display.Info().current_w*X*0.01), int(pygame.display.Info().current_h *Y*0.01)
+
+    def creer_ligne(self, x1, y1, x2, y2, epaisseur, couleur): 
         self.ligne = pygame.draw.line(self.fenetre, couleur, (x1, y1), (x2, y2), epaisseur)
         self.liste_labels.append(self.ligne)
 
+    def update(self): 
+        #[self.fenetre.blit(element, position) for element, position, taille in self.liste_labels]
+        [self.fenetre.blit(element) for element in self.liste_lignes]
+    """
     def creer_label(self, x, y, w, h, couleur):
-        self.label_xy = self.xy_pourcent(x, y)
-        self.label_wh = self.xy_pourcent(w, h)
+        self.label_xy = self.unite_relatif(x, y)
+        self.label_wh = self.unite_relatif(w, h)
         self.label = pygame.surface.Surface(self.label_wh)
         self.label.fill(couleur)
-        self.liste_labels.append((self.label, self.label_xy, self.label_wh))
+        self.liste_labels.append((self.label, self.label_xy))
+    """
+    def afficher(self, marge, longeur, epaisseur):
+        """
+        Fonction pour afficher le labyrinthe. Explication de ce bordel :
+        on a, pour chaque coordonner de point (tout en unite relatif): 
+        - la marge prise en compte (pour pouvoir deplacer tout notre labyrinthe)
+        - la longeur du trait
+        - les coordonees de i et j pour les differentes cases
+        """
+        marge_x, marge_y = self.unite_relatif(marge, marge)
+        long_mur_x, long_mur_y = self.unite_relatif(longeur, longeur)
+        epaisseur_relatif, pas_imortant  = self.unite_relatif(epaisseur, epaisseur) 
+        for i in range(self.largeur_laby):
+            for j in range(self.hauteur_laby):
+                unite_i, unite_j = self.unite_relatif(i*2, j*2)
+                if self.labyrinthe.laby[i][j].murS:
+                    self.creer_ligne(marge_x+unite_i, marge_y+unite_j+long_mur_y, marge_x+unite_i+long_mur_x, marge_y+unite_j+long_mur_y, epaisseur_relatif, green)
+                if self.labyrinthe.laby[i][j].murW:
+                    self.creer_ligne(marge_x+unite_i, marge_y+unite_j, marge_x+unite_i, marge_y+unite_j+long_mur_y, epaisseur_relatif, green)
+                if self.labyrinthe.laby[i][j].murN:
+                    self.creer_ligne(marge_x+unite_i, marge_y+unite_j, marge_x+unite_i+long_mur_x, marge_y+unite_j, epaisseur_relatif, green)
+                if self.labyrinthe.laby[i][j].murE:
+                    self.creer_ligne(marge_x+unite_i+long_mur_x, marge_y+unite_j, marge_x+unite_i+long_mur_x, marge_y+unite_j+long_mur_y, epaisseur_relatif, green)
+        
 
-    def update(self): 
-        # 
-        [self.fenetre.blit(element, position) for element, position, taille in self.liste_labels]
-        [self.fenetre.blit(element) for element in self.liste_lignes]
-            
     def boucle_jeu(self):
         while True :
             for evenement in pygame.event.get():
@@ -111,16 +139,16 @@ class Jeux():
                 print("touche s clique")
 
             
-            
+            self.afficher(20, 2, 0.1)
             self.update()
 
             pygame.display.flip() # put your work on screen
 
             self.clock.tick(60)  # limites les FPS a 60
 
-jeu = Jeux(black, "titre1")
-jeu.creer_label(500, 500, 200, 200, red)
-jeu.creer_ligne(500, 500, 100, 100, 5, green)
+jeu = Jeux(black, "titre1", 10, 20)
+#jeu.creer_label(500, 500, 200, 200, red)
+#jeu.creer_ligne(500, 500, 100, 100, 5, green)
 jeu.boucle_jeu()
 if __name__ == "__main__":
     # Lancer le programme
